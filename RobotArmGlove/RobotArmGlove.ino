76ecc9c7e08d5d8310edef633bca6fec91f2727d
gent0807 <theloopholesnk@gmail.com>
2022-05-27 16:27:55 +0900

    dddd

--- RobotArmGlove/RobotArmGlove.ino
+++ RobotArmGlove/RobotArmGlove.ino
@@ -1,18 +1,18 @@
 #include <SoftwareSerial.h>
-SoftwareSerial slave1(2,3);
-//SoftwareSerial master2(4.5);
+SoftwareSerial Slave1(2,3);
+//SoftwareSerial Master2(4.5);
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  slave1.begin(9600);
-  //master2.begin(9600);
+  Slave1.begin(9600);
+  //Master2.begin(9600);
 }
 void loop() {
   // put your main code here, to run repeatedly:
   if(Serial.available()){
-    slave1.write(Serial.read());
+    Slave1.write(Serial.read());
     }
-  if(slave1.available()){
-    Serial.write(slave1.read());
+  if(Slave1.available()){
+    Serial.write(Slave1.read());
     }
 }
