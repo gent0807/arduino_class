d340ba1ea3b67eea982e3291b360e44d581e0c66
gent0807 <theloopholesnk@gmail.com>
2022-05-27 15:53:10 +0900

    ddd

--- bluetooth/bluetooth.ino
+++ bluetooth/bluetooth.ino
@@ -1,18 +1,17 @@
 #include <SoftwareSerial.h>
-SoftwareSerial AT09(2,3);
+SoftwareSerial HC06(2,3);
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  AT09.begin(9600);
+  HC06.begin(9600);
 }
-
 void loop() {
   // put your main code here, to run repeatedly:
   if(Serial.available()){
-    AT09.write(Serial.read());
+    HC06.write(Serial.read());
     }
-  if(AT09.available()){
-    Serial.write(AT09.read());wkd
+  if(HC06.available()){
+    Serial.write(HC06.read());
     }
 }
