92318c0d3f554ff3f3854a307ae5da41dc7245bb
gent0807 <theloopholesnk@gmail.com>
2022-05-29 11:21:46 +0900

    ddd

--- RobotArmGlove/RobotArmGlove.ino
+++ RobotArmGlove/RobotArmGlove.ino
@@ -3,8 +3,8 @@ SoftwareSerial Slave1(2,3);
 //SoftwareSerial Master2(4.5);
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
   Slave1.begin(9600);
+  Serial.begin(9600);
   //Master2.begin(9600);
 }
 void loop() {
@@ -22,11 +22,12 @@ void loop() {
     }
     delay(1000);
    */
-   if(Slave1.available()){
+   /*if(Slave1.available()){
     char receive[2];
 
     Slave1.readBytes(receive, 2);
     Serial.print(receive[0]);
     Serial.println(receive[1]);
     }
+    */
 }
