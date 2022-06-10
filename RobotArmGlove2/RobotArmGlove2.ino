9a84aaddba58915cd34b527abfa8defb2c8ad08a
gent0807 <theloopholesnk@gmail.com>
2022-06-11 01:03:21 +0900

    dddddd

---
+++ RobotArmGlove2/RobotArmGlove2.ino
@@ -0,0 +1,50 @@
+#include <Wire.h>
+
+#include <SoftwareSerial.h>
+SoftwareSerial Slave1(10,3);
+
+byte sensing[]={A1,A2,A3,A4};
+void setup() {
+
+  Slave1.begin(9600);
+  Serial.begin(9600);
+
+  
+
+}
+void loop() {
+    
+    byte state[4];
+    for(int i=0;i<4;i++){
+       if(i==0){
+            byte value=analogRead(sensing[i]);
+            Serial.println(value);
+            state[i]=(byte)analogRead(sensing[i]);   
+          }
+       if(i==1){
+            byte value=analogRead(sensing[i]);
+            //Serial.println(value);
+            state[i]=(byte)analogRead(sensing[i]);
+
+          }
+       if(i==2){
+           byte value=analogRead(sensing[i]);
+           //Serial.println(value);
+           state[i]=(byte)analogRead(sensing[i]);
+
+
+             
+         }
+         if(i==3){
+            byte value=analogRead(sensing[i]);
+            //Serial.println(value);
+            state[i]=(byte)analogRead(sensing[i]);
+
+          }
+         
+          
+      }
+    
+       Slave1.write(state,4);
+ 
+}
