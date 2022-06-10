2d7e7bdbc4eb69e47ba27be66e45e82ff1e67a77
gent0807 <theloopholesnk@gmail.com>
2022-06-11 01:03:06 +0900

    dddd

--- RobotArm/RobotArm.ino
+++ RobotArm/RobotArm.ino
@@ -1,20 +1,23 @@
-   #include <DHT11.h>
-
+#include <DHT11.h>
 #include <SoftwareSerial.h>
 
+
+
 SoftwareSerial Master1(2,3);
-//SoftwareSerial Slave2(4,5);
-//const int led1=4;
-byte action[]={5,6,9};
+DHT11 dht11(8);
+
+
+int action[]={5,6,9};
 byte sensing[]={4,8};
 const int cds_output=7;
+float temp=0.0, humi=0.0;
+
 void setup() {
-  // put your setup code here, to run once:
+  
   Master1.begin(9600);
   Serial.begin(9600);
   
-  //Slave2.begin(9600);
-  //pinMode(led1, OUTPUT);
+ 
   
   for(int i=0;i<3;i++){
     pinMode(action[i], OUTPUT);
@@ -22,46 +25,22 @@ void setup() {
     
   pinMode(cds_output,OUTPUT);
   
-  for(int i=0;i<1;i++){
-    pinMode(sensing[i], INPUT);
-    }
+  pinMode(4, INPUT);
+
+
     
 }
 void loop() {
-  // put your main code here, to run repeatedly:
-  /*if(Serial.available()){
-    Master1.write(Serial.read());
-    }
-  */
-  /*if(Master1.available()){
-    char c=Master1.read();
-    Serial.write(c);
-  */  
-    
-/*Master1.println("SYS");*/
-/*char data[]={'0','K'};
-Master1.write(data,2);
-delay(1000);
-*/
-/*
-if(c=='0'){
-  digitalWrite(led1, LOW);
-  }
-else if(c=='1'){
-  digitalWrite(led1,HIGH);
-  }  
-*/
-/*if(c=='0'){
-  digitalWrite(led1, LOW);
-  }
-else if(c=='1'){
-  digitalWrite( led1, HIGH);
-  }
-}*/
+  
 byte receive[3];
 byte state[3];
 Master1.readBytes(receive,3);
 for(int i=0;i<3;i++){
+byte brightness=analogRead(A0);
+if(brightness<70){
+  digitalWrite(cds_output, HIGH);
+  }
+else digitalWrite(cds_output, LOW);
      if(i==0){
          if(receive[0]=='0'){
              if(digitalRead(sensing[0])==LOW){
@@ -84,55 +63,37 @@ for(int i=0;i<3;i++){
          
       }
       if(i==1){
+                    
            if(receive[1]=='0'){
-               if(digitalRead(sensing[1])==LOW){
-                       state[1]='1';
-               }
-               else if(digitalRead(sensing[1])==HIGH){
-                       state[1]='0';
-               }
-               digitalWrite(action[1], LOW);
+             digitalWrite(action[1], LOW);
+             if(dht11.read(humi,temp)==0){
+                    state[1]=(byte)humi;
+                    state[2]=(byte)temp;
+                }             
               
            }
            if(receive[1]=='1'){
-                if(digitalRead(sensing[1])==LOW){
-                       state[1]='1';
-                }
-                else if(digitalRead(sensing[1])==HIGH){
-                       state[1]='0';
-                }
-                digitalWrite(action[1], HIGH);
-                }
-                DHT11 dht11(sensing[1]);
-                float temp=0.0, humi=0.0;
-                if(dht11.read(humi,temp)==0){
+              digitalWrite(action[1], HIGH);
+              if(dht11.read(humi,temp)==0){
                     state[1]=(byte)humi;
                     state[2]=(byte)temp;
-                }
-            }
+                }             
+           }   
+
+          
+    
+         }
         if(i==2){
-                 if(receive[2]>=0){
+                 if(receive[2]>=0  ){
                  analogWrite(action[2], receive[2]);
             }
-            } 
+        }
+      
+        
 }
 
 Master1.write(state,3);
 
-byte brightness=analogRead(A0);
-if(brightness<60){
-  digitalWrite(cds_output, HIGH);
-  }
-else digitalWrite(cds_output, LOW);
-/*byte state[1];  
-for(int i=0;i<1;i++){
-  if(i==0&&digitalRead(sensing[i])==LOW){
-    state[i]='1';
-    }
-  else if(i==0&&digitalRead(sensing[i])==HIGH){
-    state[i]='0';
-    }
-}
-Master1.write(state,1);
-*/
+
+
 }
---
+++ RobotArm2/RobotArm2.ino
@@ -0,0 +1,89 @@
+
+#include <SoftwareSerial.h>
+#include <Servo.h>
+
+
+
+SoftwareSerial Master1(2,3);
+Servo finger1;
+Servo finger2;
+Servo finger3;
+Servo finger4;
+
+int action[]={10,11,12,13};
+
+
+
+void setup() {
+  
+  
+  
+  Master1.begin(9600);
+  Serial.begin(9600);
+
+  /*finger1.attach(10);
+  finger1.write(70);
+  finger2.attach(11);
+  finger2.write(70);
+  finger3.attach(12);
+  finger3.write(70);
+  finger4.attach(13);
+  finger4.write(70);
+  */
+
+  
+}
+void loop() {
+//byte receive[4];
+//int i;
+
+finger1.attach(10);
+finger1.write(60);
+delay(1000);
+finger1.write(180);
+delay(1000);
+
+
+/*finger1.write(0);
+delay(1000);
+
+for(i=0; i<90;i+=10){
+  finger1.write(i);
+  delay(300);
+  }
+
+delay(1000);
+*/
+/*Master1.readBytes(receive,4);*/
+
+
+/*for(int i=0;i<4;i++){
+     if(i==0){
+      byte value=receive[i];
+      value=
+      
+      
+        
+         
+      }
+    if(i==1){
+       
+       
+       }
+
+     if(i==2){
+       
+      
+
+       }          
+     if(i==3){
+       
+        
+             
+               
+        } 
+  
+    }*/
+
+
+}
